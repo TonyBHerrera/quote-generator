00d7a9c917c2c9ced76911b102e2770b7dc620b2
TonyBHerrera <tonybherrera@yahoo.com>
2021-06-15 13:18:56 -0600

    thing

---
+++ script.js
@@ -0,0 +1,25 @@
+//  Get Quotes from API
+
+let apiQuotes = [];
+
+// Show new quote
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+//  ON LOad
+
+getQuotes();
